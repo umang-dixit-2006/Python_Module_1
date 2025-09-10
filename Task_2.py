1871b478b1f4ecd3985a0c1ecbc58542275a4a2d
Umang <212006umang@gmail.com>
2025-09-10 11:01:34 +0530

    Create Task_2.py

---
+++ Task_2.py
@@ -0,0 +1,7 @@
+#Task 2
+
+firstName = input("Enter Your First Name: ")
+lastName  = input("Enter Your Last Name: ")
+fullName = firstName + ' ' + lastName
+
+print(f"Hello, {fullName}! Welcome to the Python program.")
